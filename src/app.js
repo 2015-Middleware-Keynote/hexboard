784a2ee2820ca3c08ee29c3b15d100a062f2e8f6
Brian Leathem <bleathem@gmail.com>
2015-02-10 08:07:13 -0800

    Understanding D3.js Force Layout - 1: The Simplest Possible Graph

--- src/app.js
+++ src/app.js
@@ -1,7 +1,49 @@
 'use strict';
 
 (function(d3, rx) {
-  var circle = d3.selectAll("circle");
-  circle.style("fill", "steelblue");
-  circle.attr("r", 30);
+  var width = 640
+    , height = 480;
+
+  var nodes = [
+    { x:   width/3, y: height/2 },
+    { x: 2*width/3, y: height/2 }
+  ];
+
+  var links = [
+    { source: 0, target: 1 }
+  ];
+
+  var svg = d3.select('.content').append('svg')
+    .attr('width', width)
+    .attr('height', height);
+
+  var force = d3.layout.force()
+    .size([width, height])
+    .nodes(nodes)
+    .links(links);
+
+  force.linkDistance(width/2);
+
+  var link = svg.selectAll('.link')
+    .data(links)
+    .enter().append('line')
+    .attr('class', 'link');
+
+  var node = svg.selectAll('.node')
+    .data(nodes)
+    .enter().append('circle')
+    .attr('class', 'node');
+
+  force.on('end', function() {
+    node.attr('r', width/25)
+        .attr('cx', function(d) { return d.x; })
+        .attr('cy', function(d) { return d.y; });
+
+    link.attr('x1', function(d) { return d.source.x; })
+        .attr('y1', function(d) { return d.source.y; })
+        .attr('x2', function(d) { return d.target.x; })
+        .attr('y2', function(d) { return d.target.y; });
+  });
+
+  force.start();
 })(d3, Rx);
