16c27de3e8d8dd6b88077e4e9102f43fb98f8cdb
Brian Leathem <bleathem@gmail.com>
2015-03-25 08:55:44 -0700

    Made the bower task more verbose

--- tasks/bower.js
+++ tasks/bower.js
@@ -3,9 +3,10 @@
 var bower = require('bower');
 
 module.exports = function(gulp, opts) {
-  gulp.task('bower', function(cb){
+  gulp.task('bower', function(cb) {
     bower.commands.install([], {save: true}, {})
-      .on('end', function(installed){
+      .on('end', function(installed) {
+        console.log(installed);
         cb();
       });
   });
