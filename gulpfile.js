51e970460f96d69a6abbd9815e72140a26c7d087
Brian Leathem <bleathem@gmail.com>
2015-03-25 08:07:52 -0700

    Created a bower task in gulp

--- gulpfile.js
+++ gulpfile.js
@@ -4,6 +4,7 @@ var opts = {};
 
 var opts = require('./tasks/gulp-config.js')(gulp, {});;
 
+require('./tasks/bower.js')(gulp, opts);
 require('./tasks/styles.js')(gulp, opts);
 require('./tasks/server.js')(gulp, opts);
 require('./tasks/test-backend.js')(gulp, opts);
@@ -11,4 +12,4 @@ require('./tasks/watch.js')(gulp, opts);
 
 gulp.task('default', ['serve', 'less', 'watch-client']);
 
-gulp.task('docker', ['serve']);
+gulp.task('docker', ['serve', 'bower']);
---
+++ tasks/bower.js
@@ -0,0 +1,12 @@
+'use strict';
+
+var bower = require('bower');
+
+module.exports = function(gulp, opts) {
+  gulp.task('bower', function(cb){
+    bower.commands.install([], {save: true}, {})
+      .on('end', function(installed){
+        cb();
+      });
+  });
+};
