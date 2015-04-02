fbb70fa6c6837a83249ed2ba0f8be3068224d521
Brian Leathem <bleathem@gmail.com>
2015-04-02 15:47:54 -0700

    Added a retry clause to the stomp webscoket connection

--- client/js/stomp.js
+++ client/js/stomp.js
@@ -4,6 +4,9 @@ var d3demo = d3demo || {};
 
 d3demo.stomp = (function stompFeed(d3, Rx) {
   var scans = Rx.DOM.fromWebSocket(d3demo.config.backend.ws + '/live')
+  .retryWhen(function(errors) {
+    return errors.delay(5000);
+  })
   .map(function(json) {
     return JSON.parse(json.data);
   })
