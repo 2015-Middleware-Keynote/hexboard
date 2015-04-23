084a8f5afec694a9d28e1260fedbc27c402b902c
Andres Galante <agalante@gmail.com>
2015-04-23 11:33:34 -0300

    adds patternfly to thousand visualisation and makes gulp compiles thousand less file

--- tasks/styles.js
+++ tasks/styles.js
@@ -18,4 +18,23 @@ module.exports = function(gulp, opts) {
       .pipe(less())
       .pipe(gulp.dest('./client/map/css'));
   });
+
+
+
+  // Compiles less on to /css
+  gulp.task('less', ['less-thousand']);
+  gulp.task('less-thousand', function () {
+    gulp.src('./client/thousand/less/**/*.less')
+      .pipe(plumber({
+          errorHandler: function (err) {
+              console.log(err);
+              this.emit('end');
+          }
+      }))
+      .pipe(less())
+      .pipe(gulp.dest('./client/thousand/css'));
+  });
+
+
+
 };
