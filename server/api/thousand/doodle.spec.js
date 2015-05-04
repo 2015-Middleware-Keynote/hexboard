8dd73c92181f9736908b22d351c4448f31cacaf8
Brian Leathem <bleathem@gmail.com>
2015-05-04 09:36:29 -0700

    Fixed a broken test

--- server/api/thousand/doodle.spec.js
+++ server/api/thousand/doodle.spec.js
@@ -12,7 +12,7 @@ describe('Rest API:', function () {
   describe('POST /doodle', function () {
     it('receive a file', function (done) {
       var readStream = fs.createReadStream('server/api/thousand/cherries.png');
-      var req = request(app).post('/api/doodle');
+      var req = request(app).post('/api/doodle/10');
       readStream.pipe(req, {end: false});
       readStream.on('end', function() {
         req.end(function (err, res) {
@@ -20,7 +20,7 @@ describe('Rest API:', function () {
           if (err) {
             throw new Error(err);
           }
-          res.body.should.equal('Sent!');
+          res.body.url.should.equal('http://beacon.jbosskeynote.com/api/doodle/10');
           done();
         });
       })
