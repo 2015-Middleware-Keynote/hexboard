85c8f8e9fb35114486fca8d7d84a0a78db2019a7
Brian Leathem <bleathem@gmail.com>
2015-07-22 11:52:26 -0700

    Fixed the default NAMESPACE lookup

--- server/config.js
+++ server/config.js
@@ -4,7 +4,7 @@ var autoconfig = function (config_overrides){
     APP_NAME   : process.env.APPNAME || 'sketch'
   , OAUTH_TOKEN: process.env.ACCESS_TOKEN || false
   , ALLOWED_SUBNET : process.env.ALLOWED_SUBNET || false
-  , NAMESPACE  : process.env.NAMESPACE || config.get('OPENSHIFT_BUILD_NAMESPACE')
+  , NAMESPACE  : process.env.NAMESPACE || process.env.OPENSHIFT_BUILD_NAMESPACE
   , OPENSHIFT_SERVER: process.env.OPENSHIFT_SERVER || 'openshift.default.svc.cluster.local'
   , OPENSHIFT_APP_BASENAME: process.env.APP_ROOT_URL || 'localhost:8443'
   , BASIC_AUTH_USER: process.env.BASIC_AUTH_USER || ''
