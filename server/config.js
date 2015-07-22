db18e8578f94f114727862724de395ea4ade9b8f
Brian Leathem <bleathem@gmail.com>
2015-07-22 11:25:52 -0700

    Changed the default openshift master

--- server/config.js
+++ server/config.js
@@ -5,7 +5,7 @@ var autoconfig = function (config_overrides){
   , OAUTH_TOKEN: process.env.ACCESS_TOKEN || false
   , ALLOWED_SUBNET : process.env.ALLOWED_SUBNET || false
   , NAMESPACE  : process.env.NAMESPACE || config.get('OPENSHIFT_BUILD_NAMESPACE')
-  , OPENSHIFT_SERVER: process.env.OPENSHIFT_SERVER || 'localhost:8443' // openshift.default.svc.cluster.local
+  , OPENSHIFT_SERVER: process.env.OPENSHIFT_SERVER || 'openshift.default.svc.cluster.local'
   , OPENSHIFT_APP_BASENAME: process.env.APP_ROOT_URL || 'localhost:8443'
   , BASIC_AUTH_USER: process.env.BASIC_AUTH_USER || ''
   , BASIC_AUTH_PASSWORD: process.env.BASIC_AUTH_PASSWORD || ''
