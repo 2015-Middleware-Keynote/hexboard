81b508e6da337560cea9494a0693b7854751aecd
Brian Leathem <bleathem@gmail.com>
2015-07-23 10:56:36 -0700

    Updated the README

--- scripts/post_image.manual.js
+++ scripts/post_image.manual.js
@@ -3,5 +3,6 @@
 var sketch = require('../server/hexboard/sketch')
   ;
 
-sketch.postRandomImage('localhost:9000');
+// sketch.postRandomImage('localhost:9000');
 // sketch.postRandomImage('keynote2015-hexboard.demo.router.default.local:1080');
+sketch.postRandomImage('ui.hexboard.apps.aws.paas.ninja');
