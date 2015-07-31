44e5f4b59c74de29693570719141b031fb14926b
Brian Leathem <bleathem@gmail.com>
2015-07-31 12:17:30 -0700

    Added back the scss sources

--- tasks/styles.js
+++ tasks/styles.js
@@ -1,7 +1,10 @@
 'use strict';
 
 var less    = require('gulp-less')
+  , sass    = require('gulp-sass')
   , plumber = require('gulp-plumber')
+  , minifyCss = require('gulp-minify-css')
+  , rename = require('gulp-rename')
   ;
 
 module.exports = function(gulp, opts) {
@@ -16,5 +19,19 @@ module.exports = function(gulp, opts) {
       }))
       .pipe(less())
       .pipe(gulp.dest('./static/css'));
+  });
+
+  gulp.task('sass', function(done) {
+    gulp.src('./static/mobile/scss/ionic.app.scss')
+      .pipe(sass({
+        errLogToConsole: true
+      }))
+      .pipe(gulp.dest('./static/mobile/css/'))
+      .pipe(minifyCss({
+        keepSpecialComments: 0
+      }))
+      .pipe(rename({ extname: '.min.css' }))
+      .pipe(gulp.dest('./static/mobile/css/'))
+      .on('end', done);
   });
 };
