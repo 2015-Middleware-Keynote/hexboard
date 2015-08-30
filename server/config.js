b1dc2a5b6ca4f695f94326c9fd74a5e1d6900660
ryanj <ryan.jarvinen@gmail.com>
2015-08-30 15:47:49 -0700

    adding an ADMIN_TOKEN to the app config, template #12

--- server/config.js
+++ server/config.js
@@ -2,7 +2,9 @@ var multipaas  = require('config-multipaas');
 var autoconfig = function (config_overrides){
   var config   = multipaas(config_overrides).add({
     OAUTH_TOKEN: process.env.ACCESS_TOKEN || false
-  , ALLOWED_SUBNET : process.env.ALLOWED_SUBNET || false
+  , ADMIN_TOKEN: process.env.ADMIN_TOKEN || false
+  , ALLOWED_SUBNET: process.env.ALLOWED_SUBNET || false
+  , MOBILE_ENABLED: process.env.MOBILE_ENABLED || false
   , NAMESPACE  : process.env.NAMESPACE || process.env.OPENSHIFT_BUILD_NAMESPACE || 'hexboard'
   , OPENSHIFT_SERVER: process.env.KUBERNETES_SERVICE_HOST || process.env.OPENSHIFT_SERVER || 'localhost:8443'
   , BASIC_AUTH_USER: process.env.BASIC_AUTH_USER || ''
